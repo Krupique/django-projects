856ee5f9317970695f84415bcc7b824242008eeb
Henrique <krupique@outlook.com>
2022-10-26 11:25:25 -0300

    CHORE: Aplicando deleção de tasks

--- 00-projeto_todo/todo/tasks/views.py
+++ 00-projeto_todo/todo/tasks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import HttpResponse, redirect, render, get_object_or_404, 
 from django.http import HttpResponse
 from .models import Task
 from .forms import TaskForm
+from django.contrib import messages
 
 
 def taskList(request):
@@ -41,6 +42,14 @@ def editTask(request, id):
             return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
     else:
         return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
+
+def deleteTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    task.delete()
+
+    messages.info(request, 'Tarefa deletada com sucesso.')
+
+    return redirect('/')
 
 def helloWorld(request):
     return HttpResponse('Hello World!')
