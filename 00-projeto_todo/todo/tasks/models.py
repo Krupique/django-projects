0c73e16f9a30da88712a4516a2d0255806c7b562
Henrique <krupique@outlook.com>
2022-10-26 10:35:04 -0300

    CHORE: Adicionando dados no banco.

--- 00-projeto_todo/todo/tasks/models.py
+++ 00-projeto_todo/todo/tasks/models.py
@@ -14,8 +14,8 @@ class Task(models.Model):
         choices=STATUS
     )
 
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
