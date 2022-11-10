329393da7f11e1836b69b3fc7ba74ccc986253dd
krupck <krupique@outlook.com>
2022-11-10 11:53:11 -0300

    CHORE: Finalizando parte 2 do projeto. BackEnd da aplicação

--- 01-loan_prediction/loan_predictor_api/loan_api/views.py
+++ 01-loan_prediction/loan_predictor_api/loan_api/views.py
@@ -1,28 +1,45 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.core.files.storage import FileSystemStorage
+
+import sklearn
 
 import json
 
 import pandas as pd
 
-#import joblib
-#model = joblib.load('modelPipeline.pkl')
+import joblib
+model = joblib.load('ml_model/modelPipeline-1.pkl')
 
 def scoreJson(request):
 
     data = json.loads(request.body)
 
-    dataframe = pd.DataFrame({'values': data}).T
+    dataframe = pd.DataFrame({0: data}).T
 
-    #score = model.predict_proba(dataframe)[:, -1][0]
-    #score = float(score)
-    score = 1.0
-
-    print(dataframe.values)
+    score = model.predict_proba(dataframe)[:, -1][0]
+    score = float(score)
 
     return JsonResponse({'score': score})
 
 def scoreFile(request):
-    return JsonResponse({'score': 1})
+    fileObj=request.FILES['filePath']
+    fs=FileSystemStorage()
+    filePathName=fs.save(fileObj.name,fileObj)
+    filePathName=fs.url(filePathName)
+    filePath='.'+filePathName
+
+    #columns =['Credit_History', 'LoanAmount', 'ApplicantIncome', 'CoapplicantIncome', 'Dependents']
+
+    data =pd.read_csv(filePath)
+    #data = data[columns]
+
+    score=model.predict_proba(data)[:,-1]
+
+    score={j:k for j,k in zip(data['Loan_ID'],score)}
+
+    score =sorted(score.items(),key=lambda x: x[1],reverse=True)
+
+    return JsonResponse({'result':score})
 
 
--- 01-loan_prediction/loan_predictor_api/loan_predictor_api/urls.py
+++ 01-loan_prediction/loan_predictor_api/loan_predictor_api/urls.py
@@ -5,5 +5,6 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("scoreJson/", csrf_exempt(views.scoreJson), name='score-json')
+    path("scoreJson/", csrf_exempt(views.scoreJson), name='score-json'),
+    path("scoreFile/", csrf_exempt(views.scoreFile), name='score-json')
 ]
